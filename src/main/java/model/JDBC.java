c49664b1e54e74f07f795ac36ce0e2c2ef017c94
Mrigraj <20201746@sbsstc.ac.in>
2025-06-19 00:38:34 +0530

    Revert "Added a test api call section also, let's see how it goes"
    This reverts commit 69592e82af049b1dd871e96d8e3741f2a371e535.

--- src/main/java/model/JDBC.java
+++ src/main/java/model/JDBC.java
@@ -5,28 +5,7 @@ import java.util.Vector;
 
 public class JDBC {
 
-    String sql = "SELECT \n" +
-            "  m.mail_id,\n" +
-            "  m.docket_no,\n" +
-            "  m.subject, \n" +
-            "  t.disposition_name,\n" +
-            "  t.sub_disposition_name, \n" +
-            "  t.priority_name, \n" +
-            "  t.problem_reported,\n" +
-            "  t.assigned_to_dept_name,\n" +
-            "  t.assigned_to_user_name\n" +
-            "FROM mail AS m \n" +
-            "INNER JOIN ticket_details_2025_06 AS t \n" +
-            "ON m.ticket_id = t.ticket_id \n" +
-            "WHERE \n" +
-            "  t.disposition_name IS NOT NULL AND t.disposition_name <> '' \n" +
-            "  AND t.sub_disposition_name IS NOT NULL AND t.sub_disposition_name <> '' \n" +
-            "  AND t.priority_name IS NOT NULL AND t.priority_name <> '' \n" +
-            "  AND t.problem_reported IS NOT NULL AND t.problem_reported <> '' \n" +
-            "  AND t.assigned_to_dept_name IS NOT NULL AND t.assigned_to_dept_name <> ''\n" +
-            "  AND t.assigned_to_user_name IS NOT NULL AND t.assigned_to_user_name <> ''\n" +
-            "ORDER BY m.mail_id DESC \n" +
-            "LIMIT 200";
+    String sql = "select mail_id from mail order by mail_id desc limit 10";
     String url = "jdbc:mysql://192.168.99.32:3306/crm_manager_1?useUnicode=true&characterEncoding=utf8&serverTimezone=UTC";
     String user = "db_reader_root";
     String password = "rootroot";
